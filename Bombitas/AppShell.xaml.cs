d9454c0f117d8faa58c13f00ace0a3e8dd2f5646
af3d3z <alonso.fernandez@iesnervion.es>
2024-12-13 14:37:35 +0100

    Agregar archivos de proyecto.

---
+++ Bombitas/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bombitas
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ENT/Casilla.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class Casilla : VMBase
+    {
+        private String _imagen;
+        private Boolean _esMina;
+        private Boolean _esVolteada;
+        public String Imagen { 
+            get {
+                return _imagen;
+            }
+        }
+        public Boolean EsMina { get { return _esMina; } }
+        public Boolean EsVolteada { 
+            get {
+                return _esVolteada;
+            } 
+            set {
+                if (_esVolteada && _esMina)
+                {
+                    _imagen = "anverso_mina.png";
+                }
+                else if (_esVolteada && !_esMina)
+                {
+                    _imagen = "anverso_ok.png";
+                }
+                else {
+                    _imagen = "reverso.png";
+                }
+            } 
+        }
+    }
+}
